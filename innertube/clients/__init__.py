92855f7a40b95448ff1bd17df8c790681d9f9340
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2021-02-27 10:52:31 +0000

    Move 'maps' functionality to __init__'s

--- innertube/clients/__init__.py
+++ innertube/clients/__init__.py
@@ -1,1 +1,2 @@
+from . import base
 from .clients import *
--- innertube/infos/__init__.py
+++ innertube/infos/__init__.py
@@ -3,3 +3,24 @@ from . import devices
 from . import models
 from . import services
 from . import types
+
+SERVICES = \
+{
+    key: value
+    for key in dir(services)
+    if isinstance(value := getattr(services, key), models.ServiceInfo)
+}
+
+DEVICES = \
+{
+    key: value
+    for key in dir(devices)
+    if isinstance(value := getattr(devices, key), models.DeviceInfo)
+}
+
+CLIENTS = \
+{
+    key: value
+    for key in dir(clients)
+    if isinstance(value := getattr(clients, key), models.ClientInfo)
+}
