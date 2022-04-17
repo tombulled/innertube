7fa5cc187c2cff3224c722318c6f3a139e315550
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-04-17 17:12:36 +0100

    ✨ Improve API (#26)

--- innertube/apis.py
+++ innertube/apis.py
@@ -1,34 +1,28 @@
-from typing import Optional
-import attr
-
-from . import clients, config, models
+from . import clients, config
+from .models import Context, Locale
 
 
-@attr.s(
-    auto_detect=True,
-    auto_attribs=True,
-)
 class InnerTube(clients.InnerTubeClient):
-    client: str
-    locale: Optional[models.Locale] = None
+    locale: Locale
+
+    def __init__(
+        self,
+        client_name: str,
+        locale: Locale = Locale("en", "GB"),
+    ):
+        super().__init__()
+
+        identifier: str = client_name.lower()
+
+        if identifier not in config.contexts:
+            raise ValueError(f"Unrecognised client {client_name!r}")
+
+        self.locale = locale
+        context: Context = config.contexts[identifier]
+
+        self.session.headers.update(context.headers())
+        self.session.params = self.session.params.merge(context.params())
+        self.session.context.update(context.context())
 
     def __repr__(self) -> str:
-        return f"{type(self).__name__}({self.client!r})"
-
-    def __attrs_post_init__(self) -> None:
-        super().__attrs_post_init__()
-
-        context: Optional[models.Context]
-        if context := self.context:
-            self.session.headers.update(context.headers())
-            self.session.params = self.session.params.merge(context.params())
-            self.session.context.update(context.context())
-
-    @property
-    def context(self) -> Optional[models.Context]:
-        context: models.Context
-        for context in config.contexts.values():
-            if context.client.name == self.client:
-                return context
-
-        return None
+        return f"{type(self).__name__}({self.context.client.name!r})"
