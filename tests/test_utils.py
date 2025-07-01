7caf0992bd7725c71296b90b507ac66479c59624
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2025-07-01 21:02:55 +0100

    Fix mypy static typing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,4 +2,4 @@ import innertube.utils
 
 
 def test_filter() -> None:
-    assert innertube.utils.filter({"a": None, "b": {}, "c": 123}) == dict(b={}, c=123)
+    assert innertube.utils.filter({"a": None, "b": "b", "c": "c"}) == {"b": "b", "c": "c"}
